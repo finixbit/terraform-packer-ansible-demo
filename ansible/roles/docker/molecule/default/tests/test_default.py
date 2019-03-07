f4b58ecd8952c9f6273ad501a402aa79ec1a046c
finixbit <samuelasirifi1@gmail.com>
2019-03-07 20:38:52 +0000

    add and test docker role

---
+++ ansible/roles/docker/molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_docker_is_installed(host):
+    assert host.file("/usr/bin/docker").exists
